fa39e0369790f9237c558096d4d578fa78f50f73
dimasm93 <software.dimas_m@icloud.com>
2022-04-04 10:31:09 +0700

    fixing not working

--- resource-s3/src/main/java/com/maryanto/dimas/archetype/MainApplication.java
+++ resource-s3/src/main/java/com/maryanto/dimas/archetype/MainApplication.java
@@ -1,16 +1,33 @@
 package com.maryanto.dimas.archetype;
 
+import com.maryanto.dimas.archetype.service.MinioService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableHystrix
-@EnableTransactionManagement
-public class MainApplication {
+@Slf4j
+public class MainApplication implements ApplicationRunner {
+
+    private MinioService service;
+
+    public MainApplication(MinioService service) {
+        this.service = service;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if (!this.service.isBucketExists()) {
+            log.warn("bucket not exists, we'll create for you!");
+            this.service.createdBucket();
+        }
     }
 }
